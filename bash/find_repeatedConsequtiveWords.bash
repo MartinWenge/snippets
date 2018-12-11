a9812bafe6cf442021702e54e711dc0cc0a7372e
MartinWenge <wengenmayr@ipfdd.de>
2018-12-11 11:45:26 +0100

    adding line break sensitive warning to find_repeatedConsequtiveWords script

--- bash/find_repeatedConsequtiveWords.bash
+++ bash/find_repeatedConsequtiveWords.bash
@@ -8,6 +8,20 @@ if [ -z "$filename" ]; then
     echo "no filename passed to script"
     exit 1
 else
-    grep -o -E -n '\w+' $filename | uniq -d
+    # get numbered comparison and unnumbered comparison for line break consecutive words
+    grep -o -E -n '\w+' $filename | uniq -d > 1
+    grep -o -E '\w+' $filename | uniq -d > 2
+
+    # display numbered results
+    cat 1
+
+    # warning if line break sensitive search found more words
+    if (( $(wc -l <1) < $(wc -l <2) )); then
+        echo -e "\nsome consequtive repeated words occur after linebreak!"
+    fi
+
+    # delete utility files
+    rm 1 2
+
     echo -e "\nfind repeated consequitve words script finished\n"
 fi
